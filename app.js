dfb8914800e10b901655ef8082f2dd0cab8c9a1a
Olatunbosun Adeoye <aolatunbosun11@gmail.com>
2025-01-16 10:30:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+class BankAccount {
+  constructor(holderName, balance) {
+    this.holderName = holderName;
+    this.balance = balance;
+  }
+
+  // Method to deposit money into the account
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance = this.balance + amount;
+      console.log(`${amount} deposited.`);
+    } else {
+      console.log("Deposit amount must be greater than 0.");
+    }
+  }
+
+  // Method to withdraw money if there are  sufficient funds
+  withdraw(amount) {
+    if (!amount <= 0 && this.balance >= amount) {
+      this.balance = this.balance - amount;
+      console.log(`${amount} withdrawn.`);
+    } else if (amount > 0 && this.balance < amount) {
+      console.log("Insufficient funds.");
+    } else {
+      console.log("Transaction cannot be processed");
+    }
+  }
+
+  // Method to display the current balance
+  displayBalance() {
+    console.log(`Current balance: ${this.balance}`);
+  }
+}
+
+// Example:
+const account = new BankAccount("Joe Smith", 1800);
+// account.displayBalance();
+account.deposit(500);
+account.withdraw(200);
+account.displayBalance();
